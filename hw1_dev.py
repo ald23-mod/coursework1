0dbbfa50c9f601a521629af696b7c346611cc4da
Anas <al2916@ic.ac.uk>
2018-10-24 20:41:09 +0100

    updated the loops for fitness scores and introduced the loop through years Nt

--- hw1_dev.py
+++ hw1_dev.py
@@ -3,8 +3,9 @@ Anas Lasri Doukkali, CID:01209387
 """
 import numpy as np
 import matplotlib.pyplot as plt
+from timeit import default_timer as timer
 
-
+start = timer()
 def simulate1(N,Nt,b,e):
     """Simulate C vs. M competition on N x N grid over
     Nt generations. b and e are model parameters
@@ -20,154 +21,158 @@ def simulate1(N,Nt,b,e):
 
     fc = np.zeros(Nt+1) #Fraction of points which are C
     fc[0] = S.sum()/(N*N)
-    score_M = np.zeros((N,N),dtype=float);
-    score_C = np.zeros((N,N),dtype=float);
+    score = np.zeros((N,N),dtype=float);
     Number_Neighbours = np.zeros((N,N),dtype=int);
-    for a in range(0,N):
-        for b in range(0,N):
-            if S[a,b] == 0:
-                if a == 0 and b == 0:
-                    Number_Neighbours[a,b] = 3;
-                    for c in range(a,a+2):
-                        for d in range(b,b+2):
-                            if S[c,d] == 1:
-                                score_M[a,b] = score_M[a,b] + b
-                            else:
-                                score_M[a,b] = score_M[a,b] + e
+    for t in range (1,Nt+1):
+        for a in range(0,N):
+            for b in range(0,N):
+                if S[a,b] == 0:
+                    if a == 0 and b == 0:
+                        Number_Neighbours[a,b] = 3;
+                        for c in range(a,a+2):
+                            for d in range(b,b+2):
+                                if S[c,d] == 1:
+                                    score[a,b] = score[a,b] + b
+                                else:
+                                    score[a,b] = score[a,b] + e
+                    elif a == N-1 and b == N-1:
+                        Number_Neighbours[a,b] = 3;
+                        for c in range(a-1,a+1):
+                            for d in range(b-1,b+1):
+                                if S[c,d] == 1:
+                                    score[a,b] = score[a,b] + b
+                                else:
+                                    score[a,b] = score[a,b] + e
+                    elif a == 0 and b == N-1:
+                        Number_Neighbours[a,b] = 3;
+                        for c in range(a,a+2):
+                            for d in range(b-1,b+1):
+                                if S[c,d] == 1:
+                                    score[a,b] = score[a,b] + b
+                                else:
+                                    score[a,b] = score[a,b] + e
+                    elif a == N-1 and b == 0:
+                        Number_Neighbours[a,b] = 3;
+                        for c in range(a-1,a+1):
+                            for d in range(b,b+2):
+                                if S[c,d] == 1:
+                                    score[a,b] = score[a,b] + b
+                                else:
+                                    score[a,b] = score[a,b] + e
+                    elif 0 < a < N-1  and b == 0:
+                        Number_Neighbours[a,b] = 5;
+                        for c in range(a,a+2):
+                            for d in range(b-1,b+2):
+                                if S[c,d] == 1:
+                                    score[a,b] = score[a,b] + b
+                                else:
+                                    score[a,b] = score[a,b] + e
+                    elif 0 < a < N-1  and b == N-1:
+                        Number_Neighbours[a,b] = 5;
+                        for c in range(a,a+2):
+                            for d in range(b-1,b+1):
+                                if S[c,d] == 1:
+                                    score[a,b] = score[a,b] + b
+                                else:
+                                    score[a,b] = score[a,b] + e
+                    elif a == 0 and 0 < b < N-1:
+                        Number_Neighbours[a,b] = 5;
+                        for c in range(a-1,a+2):
+                            for d in range(b,b+2):
+                                if S[c,d] == 1:
+                                    score[a,b] = score[a,b] + b
+                                else:
+                                    score[a,b] = score[a,b] + e
+                    elif a == N-1 and 0 < b < N-1:
+                        Number_Neighbours[a,b] = 5;
+                        for c in range(a-1,a+1):
+                            for d in range(b-1,b+2):
+                                if S[c,d] == 1:
+                                    score[a,b] = score[a,b] + b
+                                else:
+                                    score[a,b] = score[a,b] + e
+                    else:
+                        Number_Neighbours[a,b] = 5;
+                        for c in range(a-1,a+2):
+                            for d in range(b-1,b+2):
+                                if S[c,d] == 1:
+                                    score[a,b] = score[a,b] + b
+                                else:
+                                    score[a,b] = score[a,b] + e
+                elif S[a,b] == 1:
+                    if a == 0 and b == 0:
+                        Number_Neighbours[a,b] = 3;
+                        for c in range(a,a+2):
+                            for d in range(b,b+2):
+                                if S[c,d] == 1:
+                                    score[a,b] = score[a,b] + 1
+                    elif a == 0 and b == N-1:
+                        Number_Neighbours[a,b] = 3;
+                        for c in range(a,a+2):
+                            for d in range(b-1,b+1):
+                                if S[c,d] == 1:
+                                    score[a,b] = score[a,b] + 1
 
-                elif a == N-1 and b == N-1:
-                    Number_Neighbours[a,b] = 3;
-                    for c in range(a-1,a+1):
-                        for d in range(b-1,b+1):
-                            if S[c,d] == 1:
-                                score_M[a,b] = score_M[a,b] + b
-                            else:
-                                score_M[a,b] = score_M[a,b] + e
-                elif a == 0 and b == N-1:
-                    Number_Neighbours[a,b] = 3;
-                    for c in range(a,a+2):
-                        for d in range(b-1,b+1):
-                            if S[c,d] == 1:
-                                score_M[a,b] = score_M[a,b] + b
-                            else:
-                                score_M[a,b] = score_M[a,b] + e
-                elif a == N-1 and b == 0:
-                    Number_Neighbours[a,b] = 3;
-                    for c in range(a-1,a+1):
-                        for d in range(b,b+2):
-                            if S[c,d] == 1:
-                                score_M[a,b] = score_M[a,b] + b
-                            else:
-                                score_M[a,b] = score_M[a,b] + e
-                elif 0 < a < N-1  and b == 0:
-                    Number_Neighbours[a,b] = 5;
-                    for c in range(a,a+2):
-                        for d in range(b-1,b+2):
-                            if S[c,d] == 1:
-                                score_M[a,b] = score_M[a,b] + b
-                            else:
-                                score_M[a,b] = score_M[a,b] + e
-                elif 0 < a < N-1  and b == N-1:
-                    Number_Neighbours[a,b] = 5;
-                    for c in range(a,a+2):
-                        for d in range(b-1,b+1):
-                            if S[c,d] == 1:
-                                score_M[a,b] = score_M[a,b] + b
-                            else:
-                                score_M[a,b] = score_M[a,b] + e
-                elif a == 0 and 0 < b < N-1:
-                    Number_Neighbours[a,b] = 5;
-                    for c in range(a-1,a+2):
-                        for d in range(b,b+2):
-                            if S[c,d] == 1:
-                                score_M[a,b] = score_M[a,b] + b
-                            else:
-                                score_M[a,b] = score_M[a,b] + e
-                elif a == N-1 and 0 < b < N-1:
-                    Number_Neighbours[a,b] = 5;
-                    for c in range(a-1,a+1):
-                        for d in range(b-1,b+2):
-                            if S[c,d] == 1:
-                                score_M[a,b] = score_M[a,b] + b
-                            else:
-                                score_M[a,b] = score_M[a,b] + e
+                    elif a == N-1 and b == N-1:
+                        Number_Neighbours[a,b] = 3;
+                        for c in range(a-1,a+1):
+                            for d in range(b-1,b+1):
+                                if S[c,d] == 1:
+                                    score[a,b] = score[a,b] + 1
+                    elif a == N-1 and b == N-1:
+                        Number_Neighbours[a,b] = 3;
+                        for c in range(a,a+2):
+                            for d in range(b-1,b+1):
+                                if S[c,d] == 1:
+                                    score[a,b] = score[a,b] + 1
+                    elif a == N-1 and b == 0:
+                        Number_Neighbours[a,b] = 3;
+                        for c in range(a-1,a+1):
+                            for d in range(b,b+2):
+                                if S[c,d] == 1:
+                                    score[a,b] = score[a,b] + 1
+                    elif 0 < a < N-1  and b == 0:
+                        Number_Neighbours[a,b] = 5;
+                        for c in range(a,a+2):
+                            for d in range(b-1,b+2):
+                                if S[c,d] == 1:
+                                    score[a,b] = score[a,b] + 1
+                    elif 0 < a < N-1  and b == N-1:
+                        Number_Neighbours[a,b] = 5;
+                        for c in range(a,a+2):
+                            for d in range(b-1,b+1):
+                                if S[c,d] == 1:
+                                    score[a,b] = score[a,b] + 1
+                    elif a == 0 and 0 < b < N-1:
+                        Number_Neighbours[a,b] = 5;
+                        for c in range(a,a+2):
+                            for d in range(b-1,b+2):
+                                if S[c,d] == 1:
+                                    score[a,b] = score[a,b] + 1
+                    elif a == N-1 and 0 < b < N-1:
+                        Number_Neighbours[a,b] = 5;
+                        for c in range(a-1,a+1):
+                            for d in range(b-1,b+2):
+                                if S[c,d] == 1:
+                                    score[a,b] = score[a,b] + 1
+                    elif 0 < a < N and 0 < b < N:
+                        Number_Neighbours[a,b] = 8;
+                        for c in range(a-1,a+2):
+                            for d in range(b-1,b+2):
+                                if S[c,d] == 1:
+                                    score[a,b] = score[a,b] + 1
+                    else:
+                        print(f"fucked up at ({a},{b})")
+        for i in range(0,N):
+            for j in range(0,N):
+                if S[i,j] == 1:
+                    score[i,j] = score[i,j] - 1;
                 else:
-                    Number_Neighbours[a,b] = 5;
-                    for c in range(a-1,a+2):
-                        for d in range(b-1,b+2):
-                            if S[c,d] == 1:
-                                score_M[a,b] = score_M[a,b] + b
-                            else:
-                                score_M[a,b] = score_M[a,b] + e
-            elif S[a,b] == 1:
-                if a == 0 and b == 0:
-                    Number_Neighbours[a,b] = 3;
-                    for c in range(a,a+2):
-                        for d in range(b,b+2):
-                            if S[c,d] == 1:
-                                score_C[a,b] = score_C[a,b] + 1
-                elif a == 0 and b == N-1:
-                    Number_Neighbours[a,b] = 3;
-                    for c in range(a,a+2):
-                        for d in range(b-1,b+1):
-                            if S[c,d] == 1:
-                                score_C[a,b] = score_C[a,b] + 1
-
-                elif a == N-1 and b == N-1:
-                    Number_Neighbours[a,b] = 3;
-                    for c in range(a-1,a+1):
-                        for d in range(b-1,b+1):
-                            if S[c,d] == 1:
-                                score_C[a,b] = score_C[a,b] + 1
-                elif a == N-1 and b == N-1:
-                    Number_Neighbours[a,b] = 3;
-                    for c in range(a,a+2):
-                        for d in range(b-1,b+1):
-                            if S[c,d] == 1:
-                                score_C[a,b] = score_C[a,b] + 1
-                elif a == N-1 and b == 0:
-                    Number_Neighbours[a,b] = 3;
-                    for c in range(a-1,a+1):
-                        for d in range(b,b+2):
-                            if S[c,d] == 1:
-                                score_C[a,b] = score_C[a,b] + 1
-                elif 0 < a < N-1  and b == 0:
-                    Number_Neighbours[a,b] = 5;
-                    for c in range(a,a+2):
-                        for d in range(b-1,b+2):
-                            if S[c,d] == 1:
-                                score_C[a,b] = score_C[a,b] + 1
-                elif 0 < a < N-1  and b == N-1:
-                    Number_Neighbours[a,b] = 5;
-                    for c in range(a,a+2):
-                        for d in range(b-1,b+1):
-                            if S[c,d] == 1:
-                                score_C[a,b] = score_C[a,b] + 1
-                elif a == 0 and 0 < b < N-1:
-                    Number_Neighbours[a,b] = 5;
-                    for c in range(a,a+2):
-                        for d in range(b-1,b+2):
-                            if S[c,d] == 1:
-                                score_C[a,b] = score_C[a,b] + 1
-                elif a == N-1 and 0 < b < N-1:
-                    Number_Neighbours[a,b] = 5;
-                    for c in range(a-1,a+1):
-                        for d in range(b-1,b+2):
-                            if S[c,d] == 1:
-                                score_C[a,b] = score_C[a,b] + 1
-                elif 0 < a < N and 0 < b < N:
-                    Number_Neighbours[a,b] = 8;
-                    for c in range(a-1,a+2):
-                        for d in range(b-1,b+2):
-                            if S[c,d] == 1:
-                                score_C[a,b] = score_C[a,b] + 1
-                else:
-                    print(f"fucked up at ({a},{b})")
-
-
-
-
-    return S,fc,score_C,score_M
+                    score[i,j] = score[i,j] - e;
+        fitness_village = np.divide(score, Number_Neighbours)
+    end = timer()
+    print(end - start)
+    return S,fc,score,fitness_village
 
 def plot_S(S):
     """Simple function to create plot from input S matrix
