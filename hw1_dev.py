c6b3140b741cff25e7b71a93c0e3079b51fc9dc2
Anas <al2916@ic.ac.uk>
2018-11-01 17:13:17 +0000

    Commented version of the code. Also the most efficient so far for part a

--- hw1_dev.py
+++ hw1_dev.py
@@ -16,24 +16,43 @@ def simulate1(N, Nt, b, e):
     j = int((N-1)/2)
     S[j-1:j+2, j-1:j+2] = 0
     fc = np.zeros(Nt+1)  # Fraction of points which are C
-    fc[0] = S.sum()/(N*N)
-    A = 8*np.ones((N,N), dtype=object)
-    B = np.pad(np.zeros((N-2,N-2), dtype=object), pad_width=1, mode='constant', constant_values=3)
-    C = np.zeros((N,N))
+    fc[0] = S.sum()/(N*N) # First proportion of C's in the entire community
+    ###########################################################################
+    """ In these next 10 lines of code I will generate the Number of Neighbours
+     matrix to avoid using loops to calculate it and hence vectorize an important
+     part of the excercise.This is done by generating three matrices and using
+     simple matrix substraction to acheive the Number of Neighbours matrix for
+     any given N.
+    """
+    A = 8*np.ones((N,N), dtype=object) # Matrix of 8's
+    B = np.pad(np.zeros((N-2,N-2), dtype=object), pad_width=1, mode='constant', constant_values=3) # Matrix of 0's with 3's in the border
+    C = np.zeros((N,N)) # Matrix of zeros, with 2's in all four corners
     C[0,0] = 2
     C[0,C.shape[1]-1] = 2
     C[C.shape[0]-1,0] = 2
     C[C.shape[0]-1,C.shape[1]-1]=2
-    Number_Neighbours = A - B - C
-    Number_Neighbours = np.pad(Number_Neighbours, pad_width = 1, mode='constant', constant_values=2)
+    Number_Neighbours = A - B - C # This gives us the Number of Neighbours N*N Matrix
+    ###########################################################################
+    """ Again, to avoid excesive "if" statements, I create a border around the
+     matrices with constant value "2". This way, the loop to calculate the score
+     will be the same for all elements as all elements will have 8 "Neighbours"
+     however as we have the Number of Neighbours matrix already set, the extra
+     "2" neighbours do not affect our algorithm
+    """
     S  = np.pad(S, pad_width=1, mode='constant', constant_values=2)
+    Number_Neighbours = np.pad(Number_Neighbours, pad_width = 1, mode='constant', constant_values=2)
+    ###########################################################################
     for t in range(0,Nt):
+        # Initializing all the matrices that will be used
         score = np.zeros((N+2,N+2), dtype=object)
         fitness_matrix = np.zeros((N+2,N+2), dtype=object)
         fitness_C_comm = np.zeros((N+2,N+2), dtype=object)
         fitness_M_comm = np.zeros((N+2,N+2), dtype=object)
         transitionprob_to_M = np.zeros((N+2,N+2), dtype=object)
         transitionprob_to_C = np.zeros((N+2,N+2), dtype=object)
+
+    ###########################################################################
+        # Calculating the score of each village.
         for i in range(1,N+1):
             for j in range(1,N+1):
                 if S[i,j] == 1:
@@ -43,7 +62,7 @@ def simulate1(N, Nt, b, e):
                                 score[i,j] = score[i,j] + 1
                             elif S[c,d] == 0:
                                 score[i,j] = score[i,j]
-                    score[i,j] = score[i,j] - 1
+                    score[i,j] = score[i,j] - 1 # As the element is a C we substract 1 that was added since the loop takes into account the element as well
                 elif S[i,j] == 0:
                     for c in range(i-1,i+2):
                         for d in range(j-1,j+2):
@@ -51,55 +70,57 @@ def simulate1(N, Nt, b, e):
                                 score[i,j] = score[i,j] + b
                             elif S[c,d] == 0:
                                 score[i,j] = score[i,j] + e
-                    score[i,j] = score[i,j] - e
+                    score[i,j] = score[i,j] - e # As the element is an M we substract e that was added since the loop takes into account the element as well
 
+        # Use np.divide to divide element by element and avoid loops to
+        # calculate the fitness of each village
         fitness_matrix = np.divide(score, Number_Neighbours)
+
+        #######################################################################
+        # Setting up the loops to calculate the fitness of the "Communities"
         for i in range(1,N+1):
             for j in range(1,N+1):
                 for c in range(i-1,i+2):
                     for d in range(j-1,j+2):
                         if S[c,d] == 0:
                             fitness_M_comm[i,j] = fitness_M_comm[i,j] + fitness_matrix[c,d]
-                            fitness_C_comm[i,j] = fitness_C_comm[i,j]
                         elif S[c,d] == 1:
                             fitness_C_comm[i,j] = fitness_C_comm[i,j] + fitness_matrix[c,d]
-                            fitness_M_comm[i,j] = fitness_M_comm[i,j]
 
-                        else:
-                            fitness_C_comm[i,j] = fitness_C_comm[i,j]
-                            fitness_M_comm[i,j] = fitness_M_comm[i,j]
+        #######################################################################
+        # Setting up the loops to calculate the transition probabilities of each village
         for i in range(1,N+1):
             for j in range(1,N+1):
                 if S[i,j] == 1:
                     transitionprob_to_M[i,j] = fitness_M_comm[i,j]/(fitness_C_comm[i,j] + fitness_M_comm[i,j])
                 elif S[i,j] == 0:
                     transitionprob_to_C[i,j] = fitness_C_comm[i,j]/(fitness_M_comm[i,j] + fitness_C_comm[i,j])
-        R = np.random.rand(N+2,N+2)
+
+        # Generating a random matrix with values between 0 and 1
+        R = np.random.rand(N+1,N+1)
+        #######################################################################
+        # This is the part where the randomization happens. If the number
+        # that was randomly generated is less than the transition probability
+        # we then transition from our current state to the next one
         for i in range(1,N+1):
             for j in range(1,N+1):
                 if S[i,j] == 1:
                     if R[i,j] <= transitionprob_to_M[i,j]:
                         S[i,j] = 0
-                    else:
-                        S[i,j] = 1
                 else:
                     if R[i,j] <= transitionprob_to_C[i,j]:
                         S[i,j] = 1
-                    else:
-                        S[i,j] = 0
-        fc[t+1] = (S.sum()-8*N-8)/(N*N)
-        """
-        S = np.delete(S,0,1)
-        S = np.delete(S,N,1)
-        S = np.delete(S,0,0)
-        S = np.delete(S,N,0)
-        #fc[t] = S.sum()/(N*N)
-        """
+        # As my matrix has 2's in the border I have to introduce a correction
+        # to the proportion of C's fc vector so that it calculates the correct
+        # quantity
+        fc[t+1] = (S.sum()-8*(N+1))/(N*N)
+        # Can use the next line without '#' to plot the figures
+        #plot_S(S)
+    # Here I use slice to return S to its main form with no added borders
+    slice_val = 1
+    S = S[slice_val:-slice_val, slice_val:-slice_val]
 
-        plot_S(S)
-    return S, fc, fitness_matrix,transitionprob_to_C, transitionprob_to_M
-
-
+    return S, fc
 
 def plot_S(S):
     """Simple function to create plot from input S matrix"""
@@ -114,25 +135,44 @@ def plot_S(S):
     plt.pause(0.05)
     return None
 
-
 def simulate2(N, Nt, b, e):
-    """Simulation code for Part 2, add input variables as needed
     """
+    """
+    S = np.ones((N, N), dtype=int)  # Status of each gridpoint: 0=M, 1=C
+    j = int((N-1)/2)
+    S[j-1:j+2, j-1:j+2] = 0
+    fc = np.zeros(Nt+1)  # Fraction of points which are C
+    fc[0] = S.sum()/(N*N) # First proportion of C's in the entire community
 
+    return S, fc
 
 def analyze(display=False):
     """ Add input variables as needed
     """
-    bvalues = [1.5, 5.0, 20, 100]
-    e = 0.01
     N = 21
-    Nt = [0,21]
-    for b in enumerate(bvalues):
-        _,fc = simulate1(N,Nt,b,e)
-        print(fc)
+    Nt = 15
+    bvalues = [1.5, 2, 5.0, 20, 100]
+    fc_array = np.zeros(len(bvalues))
+    for i,b in enumerate(bvalues):
+        _,fc = simulate2(b)
+        print(i, b, fc[-1])
+        fc_array[i] = fc[-1]
+        t = np.arange(Nt+1)
+
+    p=np.polyfit(np.log(bvalues),t,1)
+
     if display:
         plt.figure()
-        plt.plot(fc[:],Nt,'x--')
+        plt.loglog(fc,Nt,'x--')
+
+        n = p[0]; A = p[1]
+        plt.plot(fc,np.exp(A)*(fc)**n,'r--')
+        plt.legend(('simulation','least-squares fit with n=%f'%(n)),loc='best')
+        plt.xlabel('M')
+        plt.ylabel('$\epsilon$')
+        plt.title('Variation of variance with sample size')
+    return fc
+
 
 
 
